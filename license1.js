2e9b2dd031cf968f89084853aeadd3c81ddce7a3
Satria Fadhil Muhammad <73277621+knightfm@users.noreply.github.com>
2020-11-29 21:52:17 +0700

    Create license1.js

---
+++ license1.js
@@ -0,0 +1,49 @@
+//<![CDATA[
+var _0x9766 = ["screen and (min-width: 60em)", "matchMedia", "matches", "onload", "sevenstar", "getElementById", "href", "location", "https://www.muagachblock.com", "setAttribute", "rel", "nofollow", "innerHTML", "Theme by <a href=\'https://www.muagachblock.com/\' style=\'display:inline-block!important;font-size:15px!important;color:#222!important;\' target=\'_blank\' title=\'Muagach Block\'>Muagach Block</a>", "reload", "masonry", ".blog-postr", "imagesLoaded", ".post-outer", "easeInOutCubic", "ready", "#blog-pager-older-link", ".blog-pager-older-link", ".post-grids", "No more pages to load.", "https://i.imgur.com/6RMhx.gif", "css", "animate", "appended", "ias"];
+var mql = window[_0x9766[1]](_0x9766[0]);
+mql[_0x9766[2]] && (window[_0x9766[3]] = function () {
+    var _0xfa54x2 = document[_0x9766[5]](_0x9766[4]);
+    null == _0xfa54x2 && (window[_0x9766[7]][_0x9766[6]] = _0x9766[8]), _0xfa54x2[_0x9766[9]](_0x9766[6], _0x9766[8]), _0xfa54x2[_0x9766[9]](_0x9766[10], _0x9766[11]), _0xfa54x2[_0x9766[12]] = _0x9766[13]
+});
+jQuery(document)[_0x9766[20]](function (_0xfa54x3) {
+    _0xfa54x3(_0x9766[16])[_0x9766[15]]({
+        itemSelector: _0x9766[18],
+        isAnimated: !0,
+        gutterWidth: 0,
+        columnWidth: 0,
+        animationOptions: {
+            duration: 750,
+            easing: _0x9766[19],
+            queue: !1
+        }
+    })[_0x9766[17]](function () {
+        _0xfa54x3(_0x9766[16])[_0x9766[15]](_0x9766[14])
+    })
+});
+$(function () {
+    var _0xfa54x3 = $(_0x9766[16]);
+    _0xfa54x3[_0x9766[17]](function () {
+        _0xfa54x3[_0x9766[15]]({
+            itemSelector: _0x9766[18],
+            columnWidth: 0
+        })
+    }), _0xfa54x3[_0x9766[29]]({
+        navSelector: _0x9766[21],
+        nextSelector: _0x9766[22],
+        itemSelector: _0x9766[23],
+        loading: {
+            finishedMsg: _0x9766[24],
+            img: _0x9766[25]
+        }
+    }, function (_0xfa54x4) {
+        var _0xfa54x5 = $(_0xfa54x4)[_0x9766[26]]({
+            opacity: 0
+        });
+        _0xfa54x5[_0x9766[17]](function () {
+            _0xfa54x5[_0x9766[27]]({
+                opacity: 1
+            }), _0xfa54x3[_0x9766[15]](_0x9766[28], _0xfa54x5, !0)
+        })
+    })
+});
+//]]>
